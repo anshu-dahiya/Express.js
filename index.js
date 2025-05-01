356e84a1d7a0e29600a21e127771fc9462a917c4
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-05-01 10:53:29 +0530

    created server by express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-jghjg
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) => {
+    // res.send("<h1>Hello Wrold!</h1>") 
+    res.status(404).send("<h1>Somthing Went Wrong</h1>");
+    
+})
+
+
+app.listen(3000,() => {
+    console.log("Server is running")
+})
