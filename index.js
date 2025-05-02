bb8780d20f0eafd242600aad9170c9b322dc363e
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-05-02 18:28:08 +0530

    create routes by express

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/',(req,res) => {
-    // res.send("<h1>Hello Wrold!</h1>") 
-    res.status(404).send("<h1>Somthing Went Wrong</h1>");
-    
+    res.send("<h1>Hello Wrold!</h1>") 
+})
+app.get('/about',(req,res) => {
+    res.send("<h1>About Page</h1>") 
+})
+app.get('/files',(req,res) => {
+    res.sendFile(path.join(__dirname,'./index.html')) 
+})
+app.get('/jsonres',(req,res) => {
+    res.json([{
+        firstName: 'John',
+        lastName: 'Cena'
+    },
+    {
+        firstName: 'Cody',
+        lastName: 'Rhodes'
+    }])
+})
+
+app.all('*',(req,res) => {          //app.all used for all urls that are not by us
+    res.send("Page Not Found")
 })
 
 
