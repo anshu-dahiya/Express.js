2954496390f104216bb2ddab700424a1427c6314
Anshu Dahiya <157293324+anshu-dahiya@users.noreply.github.com>
2025-05-07 19:40:11 +0530

    Post, Put, Delete Methods

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
+//Test Post,Put,Delete of data by use of (Thunder_Client or Postman)
 const express = require('express');
 const app = express();
-const morgan = require('morgan')  //morgan is buildIn middleware
+
+app.use(express.json())
 
 
-app.use(morgan('dev'));       //using middleware
+let books = [
+    { id:1, title:'Book 1', author:'Auhtor 1'},
+    { id:2, title:'Book 2', author:'Auhtor 2'}
+]    
 
-app.get('/',(req,res) => {
-    res.send("Home");
+//Get Method
+app.get('/books',(req,res) => {
+    res.json(books)
 })
 
-app.get('/about',(req,res) => {
-    res.send("About Page");
+//Post Method 
+app.post('/books',(req,res) => {
+    console.log(req.body);
+    const newBook = req.body;
+    newBook.id = books.length + 1;
+    books.push(newBook);
+
+    res.status(201).json(books);
 })
 
-app.get('/contact',(req,res) => {
-    res.send("Contact Page");
+
+//Put Method
+app.put('/books/:id',(req,res) => {
+    const id = parseInt(req.params.id);
+    const updatedBook = req.body;
+    const index = books.findIndex(book => book.id === id);
+
+    if(index !== -1){
+        books[index] = {...books[index], ...updatedBook};
+        res.json(books[index]);
+    }
+    else{
+        res.status(404).json({error:"Book Not Found"});
+    }
 })
+
+
+//Delete Method
+app.delete('/books/:id',(req,res) => {
+    const id = parseInt(req.params.id);
+    const index = books.findIndex(book => book.id === id);
+
+    if(index !== -1){
+        const deletedBook = books[index];
+        books.splice(index,1);   //removes exactly 1 element from books at position specified by index.
+        res.json(deletedBook);
+    }
+    else{
+        res.status(404).json({error:"Book Not Found"});
+    }
+})
+
+
+
 
 app.listen(3000, () => {
     console.log("Server Started")
